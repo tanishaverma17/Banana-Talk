51cc0b86d35b14fbd4fb40446375b2eff8e37b79
tanishaverma17 <76125836+tanishaverma17@users.noreply.github.com>
2023-07-09 21:53:06 +0530

    add-Input and Output
    added functionality that input and output

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 // var userName = prompt("Enter your name?")
 // console.log("Hello "+ userName);
 // alert("hello");
-var btnTranslate = document.querySelector("btn-translate");
+var btnTranslate = document.querySelector("#btn");
+var text = document.querySelector("#text");  
+var OutputDiv = document.querySelector("#output");
+
+// OutputDiv.innerText = "Tanisha Verma"; //output
 btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked");
+    // console.log("input", textInput.value) //input
+    OutputDiv.innerText = "aaaaaa" + text.value;
 });
