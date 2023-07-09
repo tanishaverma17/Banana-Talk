ef48fa31269dab05be83140b0c0193d311b7b593
tanishaverma17 <76125836+tanishaverma17@users.noreply.github.com>
2023-07-09 20:21:53 +0530

    added-Js
    feature onclick added

---
+++ script.js
@@ -0,0 +1,9 @@
+// console.log("hello");
+// alert("this page works right")
+// var userName = prompt("Enter your name?")
+// console.log("Hello "+ userName);
+// alert("hello");
+var btnTranslate = document.querySelector("btn-translate");
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked");
+});
