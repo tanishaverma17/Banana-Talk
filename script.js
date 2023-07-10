2fd66a64f38f2c1c3c33230c1ab9571c93445c29
tanishaverma17 <76125836+tanishaverma17@users.noreply.github.com>
2023-07-10 19:28:54 +0530

    updated: html and js
    JavaScript part done

--- script.js
+++ script.js
@@ -6,9 +6,33 @@
 var btnTranslate = document.querySelector("#btn");
 var text = document.querySelector("#text");  
 var OutputDiv = document.querySelector("#output");
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+function getTranslation(text){
+    return serverURL +"?" + "text="+text;
+}
+
+function errorHandler(error){
+    console.log("Error occured:" + error)
+} 
+
+function clickHandler(){
+    var InputText = text.value; //taking input
+    //calling server for processing
+    fetch(getTranslation(InputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated; 
+        OutputDiv.innerText = translatedText;
+        console.log(json.contents.translated)
+    })
+    .catch(errorHandler)
+}
 // OutputDiv.innerText = "Tanisha Verma"; //output
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    // console.log("input", textInput.value) //input
-    OutputDiv.innerText = "aaaaaa" + text.value;
-});
+btnTranslate.addEventListener("click", clickHandler);
+
+// console.log("input", textInput.value) //input
+// fetch(url)
+// .then(response => response.json())
+// .then(json => console.log(json))
